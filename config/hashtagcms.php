0f8047c71b79c898cd6699a6411a36a845a22b3d
marghoobsuleman <marghoobsuleman@yahoo.com>
2023-03-19 16:35:48 +0530

    close to release

--- config/hashtagcms.php
+++ config/hashtagcms.php
@@ -9,15 +9,16 @@ return [
         'base_path' => '',
         'theme' => 'fe.default',
         /***
-         * In case you want to setup different CDN for diffrent site
+         * In case you want to set up a different CDN
+         * for the different site
          */
-        /*'assets_path'=>array("dev.hashtagcms.com"=>array('base_url'=>env('ASSET_URL', ''),
+        /*'assets_path'=>array("dev.hashtagcms.com"=>array('base_url'=>env('ASSET_URL', 'https://images.hashtagcms.org/media'),
                                     'base_path'=>'/assets/hashtagcms/fe',
                                     'js'=>'js', 'css'=>'css', 'image'=>'img')
         ),*/
         'assets_path'=>array('base_url'=>env('ASSET_URL', ''),
-                            'base_path'=>'/assets/hashtagcms/fe',
-                            'js'=>'js', 'css'=>'css', 'image'=>'img'),
+            'base_path'=>'/assets/hashtagcms/fe',
+            'js'=>'js', 'css'=>'css', 'image'=>'img'),
         'media_path' => '/storage/media',
         'records_per_page' => 20
     ],
@@ -26,25 +27,36 @@ return [
         "http_path"=>"/storage/media"
     ],
     "message"=> [
-        "themeNotDefined"=>"<div style='margin: 100px; text-align: center; font-size: 70px; color:#ffffff'>Theme is no theme defined for this category</div>",
         "zeroModuleSelected"=>"<div style='margin: 100px; text-align: center; font-size: 70px; color:mediumvioletred'>There is no module assigned for this category</div>"
     ],
     "redirect_with_message_design"=> [
-        'css_success'=>'alert-success alert mb-0 appear',
+        'css_success'=>'alert alert-primary mb-0 appear',
         'css_error'=>'alert-danger alert mb-0 appear',
         'css_error_close_button'=>'fa fa-times',
         'error_close_text'=>''
     ],
-    "default"=>array("lang"=>"en", "platform"=>"web", "when_default_category_not_found"=>"home"),
-    "domains" => array(
-        "dev.hashtagcms.com"=>env('CONTEXT', 'hashtagcms')
-    ),
     "blog_per_page"=>10,
-    "more_categories_on_blog_listing"=>array(), //"support"
+    "more_categories_on_blog_listing"=>array("support"), //"support"
+
+    /**
+     * This is needed to run same site on multiple domain
+     * and when you want to run via external api you need to have
+     * domain and context key combination here to run the site.
+     */
+    "domains" => array(
+        "dev.hashtagcms.com"=>env('CONTEXT', 'jflcms'),
+        "api.dominos.co.in"=>'jflcms',
+        "api.hashtagcms.com"=>'htcms'
+
+    ),
     "api_secrets"=>array(
         /**
-         * This is a combination of context and random key. will be used in /api/hashtagcms/public/configs/v1/site-configs?site=htcmsctx&api_secret=61c58507bbac1
+         * Do not forget the change this key when you go in production
+         * This is a combination of context and random key.
+         * will be used in external ie /api/hashtagcms/public/configs/v2/site-configs?site=htcmsctx&api_secret=61d2aeb489bb8df385
+         * api_key as header will be set with the same value
          */
-        "htcmsctx"=>"61c58507bbac1"
+        "htcms"=>"61c58507bbac1",
+        "jflcms"=>"d2aeb489bb8df385"
     )
 ];
--- config/hashtagcmsadmin.php
+++ config/hashtagcmsadmin.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'cmsInfo'=> [
         'defaultPage'=>'dashboard',
@@ -8,28 +7,28 @@ return [
         'base_path' => '/admin',
         'version' => env('BE_RESOURCE_VERSION', '310120221229'),
         'theme' => 'hashtagcms::be.neo',
-        'theme_assets' => 'build/assets',
+        'theme_assets' => 'assets/hashtagcms/be/neo',
         'app_url' => env("APP_URL"),
-        'resource_dir' => 'be/default',
+        'resource_dir' => 'be/neo',
         'media_path' => '/storage/media', //media path
         'show_delete_popup' => false,
         'records_per_page' => 20,
         'action_field_title' => array("label"=>"Action", "key"=>"action"),
         'action_as_ajax' => array('delete', 'approve', 'publish_status'),
         'make_field_as_link'=>array(array("key"=>"id", "action"=>"edit"),
-                                    array("key"=>"publish_status", "action"=>"publish",
-                                        "css_0"=>"text-warning fa fa-circle-o", "css_1"=>"text-success fa fa-check-square-o"),
-                                    ),
+            array("key"=>"publish_status", "action"=>"publish",
+                "css_0"=>"text-warning fa fa-circle-o", "css_1"=>"text-success fa fa-check-square-o"),
+        ),
         'action_icon_css'=>array(
             'edit'=>'fa fa-edit',
             'delete'=>'fa fa-trash-o',
             'approve'=>'glyphicon glyphicon-ok',
             'loading'=>'fa-spinner fa-pulse fa-fw'
         ),
-        'module_types'=>array('Static','Query','Service','Custom','QueryService','UrlService')
-      ],
+        'module_types'=>array('Static','Query','Service','Custom','QueryService','UrlService', 'MenuService')
+    ],
     "media" => [
-        "upload_path"=>"public/media", //-- /storage/app/public/media - run - php artisan storage:link
+        "upload_path"=>"public/media", // /storage/app/public/media  >_ php artisan storage:link
     ],
     "imageSupportedByBrowsers" => array("apng", "avif", "gif", "jpg","jpeg", "jfif", "pjpeg", "pjp", "png", "svg", "webp", "bmp", "ico", "cur", "tif", "tiff"),
     "chartPages"=>10
