317c799ca33aab83cb1967f2b22d637cd8966358
Marghoob Suleman <marghoobsuleman@Marghoobs-MacBook-Pro.local>
2024-03-17 19:45:22 +0530

    Laravel 11 support

--- database/migrations/2018_12_21_094621_create_sessions_table.php
+++ database/migrations/2018_12_21_094621_create_sessions_table.php
@@ -13,14 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id', 100)->unique();
-            $table->bigInteger('user_id', false, true)->nullable();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->text('payload');
-            $table->integer('last_activity');
-        });
+        if(!Schema::hasTable('sessions')) {
+            Schema::create('sessions', function (Blueprint $table) {
+                $table->string('id', 255)->unique();
+                $table->bigInteger('user_id', false, true)->nullable();
+                $table->string('ip_address', 45)->nullable();
+                $table->text('user_agent')->nullable();
+                $table->text('payload');
+                $table->integer('last_activity');
+            });
+        }
+
     }
 
     /**
